c9f0af303696cdbe26e664774bd652c9868852e5
Cyril-Dias <105729835+Cyril-Dias@users.noreply.github.com>
2022-07-21 14:44:33 +0200

    file name

--- main.js
+++
@@ -1,3 +0,0 @@
-  <script>
-    alert('Bonjour et bienvenue ! Ce site est en cours de programmation, il sera amené à  changer.')
-  </script>
---
+++ script.js
@@ -0,0 +1,3 @@
+<script>
+  alert('Bonjour et bienvenue ! Ce site est en cours de programmation, il sera amené à  changer.')
+</script>
