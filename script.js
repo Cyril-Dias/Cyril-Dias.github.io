55c10d50ab0da61e328ea9f6a7ede49c593f64c5
Cyril-Dias <105729835+Cyril-Dias@users.noreply.github.com>
2022-07-21 14:49:22 +0200

    Update script.js
    Undown the HTML <>

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-<script>
-  alert('Bonjour et bienvenue ! Ce site est en cours de programmation, il sera amené à  changer.')
-</script>
+alert('Bonjour et bienvenue ! Ce site est en cours de programmation, il sera amené à  changer.')
+
